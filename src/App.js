dfdd5ce198fca56287ac0bb4b1161064225d96d6
Okera Johnson <okerajohnson@gmail.com>
2024-02-14 17:14:01 -0500

    trying to ignore the git before i get got

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  }
+});
 
 const TITLE = 'React GraphQl Github Client';
 
 class App extends Component {
+  state = {
+    path: 'the-road-to-learn-react/the-road-to-learn-react',
+  };
+
+  componentDidMount() {
+    // fetch data
+  }
+
+  onChange = event => {
+    this.setState({ path: event.target.value });
+  };
+
+  onSubmit = event => {
+    //fetch data
+  
+
+  event.preventDefault();
+  };
+
   render () {
+    const { path } = this.statel
     return (
       <div>
         <h1>
           {TITLE}
         </h1>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">
+            Show open issues for https://github.com/
+             </label>
+             <input
+               id="url"
+               type="text"
+               value={path}
+               onChange={this.onChange}
+               style={{ width: '300px' }}
+               />
+               <button type="submit">Search</button>
+        </form>
+        <hr />
+
+        {/* here comes result */}
       </div>
     );
   }
